3791b56c2daa4d992f16c7776badd9bedd538d07
Assylzhan <assylzhan.baimuratov@gmail.com>
2023-11-08 12:43:23 +0500

    add user-service

--- src/app/tokens/tokens.ts
+++ src/app/tokens/tokens.ts
@@ -1,4 +1,4 @@
-import {UserService} from "@ser";
+import {UserService} from "@services/user.service";
 import {InjectionToken} from "@angular/core";
 
 export const USER_SERVICE = new InjectionToken<UserService>('USER_SERVICE')
